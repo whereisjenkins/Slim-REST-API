af164cae8a1d500d5759685607cfcd8c865672cf
Jenkins E <8914235+whereisjenkins@users.noreply.github.com>
2018-11-26 11:39:15 -0500

    Add & Retrieve
    Routes for adding and retrieving customer information

--- src/config/db.php
+++ src/config/db.php
@@ -11,7 +11,7 @@ class db{
 	private $dbname = 'slim';
 	
 	// Connect to database
-	public function connect90{
+	public function connect(){
 		$mysql_connect_str = "mysql:host=" . $this->dbhost . ";dbname=" . $this->dbname . "";
 		$dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
 		$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- src/routes/customers.php
+++ src/routes/customers.php
@@ -6,5 +6,72 @@ $app = new \Slim\App;
 
 // Get All Customers
 $app->get('/api/customers', function(Request $request, Response $response){
-	echo "Customers";
+	$sql = "SELECT * FROM customers";
+	
+	try{
+		$db = new db();
+		$db = $db->connect();
+		
+		$stmt = $db->query($sql);
+		$customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$db = null;
+		echo json_encode($customers);
+	} catch(PDOException $e){
+		echo '{"error" : {"text": ' . $e->getMessage() . '}'; 
+	}
+});
+
+// Get a Single Customer
+$app->get('/api/customer/{id}', function(Request $request, Response $response){
+	$id = $request->getAttribute('id');
+	
+	$sql = "SELECT * FROM customers WHERE id = $id";
+	
+	try{
+		// Get DB Object
+		$db = new db();
+		$db = $db->connect();
+		
+		$stmt = $db->query($sql);
+		$customer = $stmt->fetch(PDO::FETCH_OBJ);
+		$db = null;
+		echo json_encode($customer);
+	} catch(PDOException $e){
+		echo '{"error": {"text": '. $e->getMessage() . '}';
+	}
+});
+
+// Add Customer
+$app->post('/api/customer/add', function(Request $request, Response $response){
+	$first_name 	= $request->getParam('first-name');
+	$last_name		= $request->getParam('last_name');
+	$phone			= $request->getParam('phone');
+	$email 			= $request->getParam('email');
+	$address 		= $request->getParam('address');
+	$city 			= $request->getParam('city');
+	$state 			= $request->getParam('state');
+	
+	$sql = "INSERT INTO customers (FIRST_NAME, LAST_NAME, PHONE, EMAIL, ADDRESS, CITY, STATE)
+	VALUES (:first_name, :last_name, :phone, :email, :address, :city, :state)";
+	
+	try{
+		$db = new db();
+		$db = $db->connect();
+		
+		$stmt = $db->prepare($sql);
+		
+		$stmt->bindParam(':first_name', $first_name);
+		$stmt->bindParam(':last_name', 	$last_name);
+		$stmt->bindParam(':phone', 		$phone);
+		$stmt->bindParam(':email', 		$email);
+		$stmt->bindParam(':address', 	$address);
+		$stmt->bindParam(':city', 		$city);
+		$stmt->bindParam(':state', 		$state);
+		
+		$stmt->execute();
+		
+		echo '{"notice": {"text": "Customer Added"}';
+	} catch(PDOException $e){
+		echo '{"error": {"text": ' . $e->getMessage() . '}';
+	}
 });
